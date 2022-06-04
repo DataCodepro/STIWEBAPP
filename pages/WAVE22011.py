22327884b885f1df772ec786ea42b76cc7cdd4e9
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2022-06-04 15:14:12 +0100

    Update WAVE22011.py

--- pages/WAVE22011.py
+++ pages/WAVE22011.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
-import streamlit_theme as stt
+
 
 def wave2():
     df = pd.read_csv('cleaneddata2.csv')
