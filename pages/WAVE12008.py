b8092c247afdb49fb0df70b8506e89d295ea1390
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2022-06-05 14:12:02 +0100

    Update WAVE12008.py

--- pages/WAVE12008.py
+++ pages/WAVE12008.py
@@ -6,53 +6,53 @@ import seaborn as sns
 import plotly.express as px
 def wave1():
     df = pd.read_csv('cleaneddata2.csv')
-    select = st.sidebar.selectbox("EXPLORATORY DATA ANALYSIS WAVE1 2008",['TOTAL NUMBER OF STAFFS IN YEAR 2005 BY SECTORS ACORDING TO THEIR AREA OF SERVICES',
-    'TOTAL NUMBER OF STAFFS IN YEAR 2006 BY SECTORS ACORDING TO THEIR AREA OF SERVICES',
-        'TOTAL NUMBER OF STAFFS IN YEAR 2007 BY SECTORS ACORDING TO THEIR AREA OF SERVICES',
-        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICES',
-        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICES',
-        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICES'],key =1)
-    if select == 'TOTAL NUMBER OF STAFFS IN YEAR 2005 BY SECTORS ACORDING TO THEIR AREA OF SERVICES':
-        st.subheader('TOTAL NUMBER OF STAFFS IN YEAR 2005 BY SECTORS ACORDING TO THEIR AREA OF SERVICES',)
+    select = st.sidebar.selectbox("EXPLORATORY DATA ANALYSIS WAVE1 2008",['TOTAL NUMBER OF STAFF IN YEAR 2005 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE',
+    'TOTAL NUMBER OF STAFF IN YEAR 2006 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE',
+        'TOTAL NUMBER OF STAFF IN YEAR 2007 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE',
+        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICE',
+        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICE',
+        'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICE'],key =1)
+    if select == 'TOTAL NUMBER OF STAFF IN YEAR 2005 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE':
+        st.subheader('TOTAL NUMBER OF STAFF IN YEAR 2005 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE',)
         fig = px.sunburst(df, path=['service', 'sector'], values='totalstaff05',width=800, height=500)
         fig.update_traces(textinfo='label+percent entry')
         fig.update_layout(margin= dict(l=20, r=20, t=20, b=20),paper_bgcolor="#4233FF",)
         st.plotly_chart(fig)
-    elif select == 'TOTAL NUMBER OF STAFFS IN YEAR 2006 BY SECTORS ACORDING TO THEIR AREA OF SERVICES':
-                st.subheader('TOTAL NUMBER OF STAFFS IN YEAR 2006 BY SECTORS ACORDING TO THEIR AREA OF SERVICES')
+    elif select == 'TOTAL NUMBER OF STAFF IN YEAR 2006 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE':
+                st.subheader('TOTAL NUMBER OF STAFF IN YEAR 2006 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE')
                 fig = px.sunburst(df, path=['service', 'sector'], values='totalstaff06',width=800, height=500)
                 fig.update_traces(textinfo='label+percent entry')
                 fig.update_layout(margin= dict(l=20, r=20, t=20, b=20),
                                     paper_bgcolor="#4233FF ",)
                 st.plotly_chart(fig)
-    elif select == 'TOTAL NUMBER OF STAFFS IN YEAR 2007 BY SECTORS ACORDING TO THEIR AREA OF SERVICES':
-                st.subheader('TOTAL NUMBER OF STAFFS IN YEAR 2007 BY SECTORS ACORDING TO THEIR AREA OF SERVICES')
+    elif select == 'TOTAL NUMBER OF STAFF IN YEAR 2007 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE':
+                st.subheader('TOTAL NUMBER OF STAFF IN YEAR 2007 BY SECTORS ACCORDING TO THEIR AREA OF SERVICE')
                 fig = px.sunburst(df, path=['service', 'sector'], values='totalstaff07',width=800, height=500,color_continuous_scale='mint')
                 fig.update_traces(textinfo='label+percent entry')
                 fig.update_layout(margin= dict(l=20, r=20, t=20, b=20),
                                     paper_bgcolor="#4233FF ",)
                 st.plotly_chart(fig)
-    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICES':
+    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICE':
                 df = df[df['turnover05']!=0]
-                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICES')
+                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2005 FOR EACH SECTOR GROUPED BY THEIR SERVICE')
                 fig = px.sunburst(df, path=['service', 'sector'], values='turnover05',width=800, height=500,color='turnover05', 
                 color_continuous_scale='balance',color_continuous_midpoint=np.mean(df['turnover05']))
                 fig.update_traces(textinfo='label+percent entry') 
                 fig.update_layout(margin= dict(l=20, r=20, t=20, b=20),
                                     paper_bgcolor="#4233FF ",)
                 st.plotly_chart(fig)
-    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICES':
+    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICE':
                 df = df[df['turnover06']!=0]
-                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICES')
+                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2006 FOR EACH SECTOR GROUPED BY THEIR SERVICE')
                 fig = px.sunburst(df, path=['service', 'sector'], values='turnover06',width=800, height=500,color='turnover06', 
                 color_continuous_scale='redor',color_continuous_midpoint=np.mean(df['turnover06']))
                 fig.update_traces(textinfo='label+percent entry') 
                 fig.update_layout(margin= dict(l=20, r=20, t=20, b=20),
                                     paper_bgcolor="#4233FF ",)
                 st.plotly_chart(fig)
-    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICES':
+    elif select == 'TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICE':
                 df = df[df['turnover07']!=0]
-                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICES')
+                st.subheader('TOTAL AND AVERAGE TURNOVER FOR THE YEAR 2007 FOR EACH SECTOR GROUPED BY THEIR SERVICE')
                 fig = px.sunburst(df, path=['year','service', 'sector'], values='turnover07',width=800, height=500,color='turnover07', 
                 color_continuous_scale='rainbow',color_continuous_midpoint=np.mean(df['turnover07']))
                 fig.update_traces(textinfo='label+percent entry') 
