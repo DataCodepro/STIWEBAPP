00978e23d93610821ac4b15e143927c23f04cdd2
Simeon-paul Taiwo Emmanuel <65008596+Emmanuel2202@users.noreply.github.com>
2022-06-09 10:34:59 +0100

    Update dataframe.py

--- pages/dataframe.py
+++ pages/dataframe.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 def data_frame_demo():
    df = pd.read_csv('cleaneddata2')
-   return st.write(df)
+   return df
 
 st.set_page_config(page_title="DataFrame", page_icon="📊")
 st.markdown("# DataFrame")
